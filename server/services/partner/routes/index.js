07851e93ef7f49c1bc9e5b934eeef1c8ad864c52
rezadulhaq <dhiaulhaqreza@gmail.com>
2023-02-03 00:44:49 +0700

    add partner register, partner login, use helper bcrypt and helper jwt

--- server/services/partner/routes/index.js
+++ server/services/partner/routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const UserController = require("../controllers/userController");
 const router = express.Router();
 
+router.post("/partner/register", UserController.register);
+router.post("/partner/login", UserController.login);
 router.get("/", (req, res) => {
     res.send("Hello World!");
 });
