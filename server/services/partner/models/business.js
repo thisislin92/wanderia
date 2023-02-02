c576872701374102c3807f8b27f80f87d4344a1f
rezadulhaq <dhiaulhaqreza@gmail.com>
2023-02-02 21:33:44 +0700

    migration table categories, partners, businesses, posts, and seeding categories

---
+++ server/services/partner/models/business.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Business extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Business.init({
+    name: DataTypes.STRING,
+    latitude: DataTypes.FLOAT,
+    longitude: DataTypes.FLOAT,
+    description: DataTypes.TEXT,
+    mapUrl: DataTypes.STRING,
+    CategoryId: DataTypes.INTEGER,
+    PartnerId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Business',
+  });
+  return Business;
+};
