4bfc7439f865dd1afbfebbdf1441573cc78fede4
rezadulhaq <dhiaulhaqreza@gmail.com>
2023-02-04 15:49:41 +0700

    add authentication and authorization

--- server/services/partner/routes/index.js
+++ server/services/partner/routes/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const BusinessController = require("../controllers/businesController");
 const CategoryController = require("../controllers/categoryController");
 const UserController = require("../controllers/userController");
+const { authentication, authorization } = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/partner/register", UserController.register);
 router.post("/partner/login", UserController.login);
+router.use(authentication);
 router.post("/business", BusinessController.createBusiness);
-router.patch("/business/:id", BusinessController.editBusiness);
+router.patch("/business/:id", authorization, BusinessController.editBusiness);
 router.get("/categories", CategoryController.readCategory);
 
 module.exports = router;
